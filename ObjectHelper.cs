a48a2311c5700704f1ee4b85e488f0b9cad2724c
Norman Paniagua <normanpaniagua@gmail.com>
2015-01-29 20:28:07 -0300

    Initial commit

---
+++ ObjectHelper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+
+namespace WSD
+{
+	public class ObjectHelper
+	{
+		static public object GetProperty(object obj, string propertyName, object defaultValue = null)
+		{
+			PropertyInfo property = obj.GetType ().GetRuntimeProperty (propertyName);
+
+			return property != null ? (
+				property.GetValue(obj, null) != null ? property.GetValue(obj, null) : defaultValue
+			) : defaultValue;
+		}
+
+		static public Dictionary<string, object> GetProperties (object obj)
+		{
+			IEnumerable<PropertyInfo> properties = obj.GetType ().GetRuntimeProperties ();
+
+			Dictionary<string, object> props = new Dictionary<string, object> ();
+
+			var enumerator = properties.GetEnumerator ();
+
+			while (enumerator.MoveNext ()) {
+				PropertyInfo property = enumerator.Current;
+				props.Add (property.Name, property.GetValue (obj));
+			}
+
+			return props;
+		}
+
+		static public Dictionary<string, T> GetProperties<T> (object obj)
+		{
+			Dictionary<string, object> _properties = GetProperties (obj);
+			Dictionary<string, T> properties = new Dictionary<string, T> ();
+
+			foreach (KeyValuePair<string, object> row in _properties) {
+				properties.Add (row.Key, (T) row.Value);
+			}
+
+			return properties;
+		}
+	}
+}
---
+++ Readme.md
@@ -0,0 +1,17 @@
+WSD
+---
+
+Core library, no real use as standalone
+
+Using it
+---
+
+- [Data](http://github.com/norman784/wsd-data)
+- [Rest](http://github.com/norman784/wsd-rest)
+
+The main goal is to have a library that leverage the rest api comunication, so I ended up with this library.
+
+Why I build this this library?
+---
+
+Because here I will expose all the libs that compose WSD, now it is called WSD Rest Api but latter on I will add all the libs that write because I'm lazy to do it every time the same thing, or because my web background doesn't allow me to do the desktop/mobile like development way.
